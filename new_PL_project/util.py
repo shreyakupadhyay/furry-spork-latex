59423a8dd8e431e3fa4e7d0494a6ae2efb872720
shreyakupadhyay <shreyakupadhyay07@gmail.com>
2017-05-08 00:11:40 +0530

    added coordinates specific latex code generation

--- new_PL_project/util.py
+++ new_PL_project/util.py
@@ -1,12 +1,20 @@
+import numpy as np
 import sys
 import genLatexCode
 import lineSegments
 from shapely.ops import polygonize
+from shapely.geometry import mapping
 
 if __name__ == "__main__":
 	lines = lineSegments.main(sys.argv[1])
-
+	all_shapes = ""
 	for i in (list(polygonize(lines))):
-		print i.envelope
-		
-	genLatexCode.latexCode(lines)
+		points = mapping(i.envelope)
+		coordinates = ""
+		for i in range(0,len(points["coordinates"][0])):
+			coordinates = coordinates + str(points["coordinates"][0][i]) + " -- "
+		coordinates = "\draw[orange, ultra thick] "+ coordinates + "cycle;"
+		# print coordinates		
+		all_shapes = all_shapes + coordinates + "\n"
+	print all_shapes
+	genLatexCode.latexCode(all_shapes)
